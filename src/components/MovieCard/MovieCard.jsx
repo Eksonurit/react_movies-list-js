661b79c9f69afe10f2218c5c9ac11d41559dabe2
Eksonurit <fizervitalij897@gmail.com>
2025-08-05 22:37:09 +0300

    add logo link

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -12,7 +12,9 @@ export const MovieCard = ({ movie }) => (
       <div className="media">
         <div className="media-left">
           <figure className="image is-48x48">
-            <img src="images/imdb-logo.jpeg" alt="imdb" />
+            <a href={movie.imdbUrl} data-cy="MovieLink">
+              <img src="images/imdb-logo.jpeg" alt="imdb" />
+            </a>
           </figure>
         </div>
 
